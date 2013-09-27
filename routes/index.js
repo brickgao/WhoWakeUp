71aad39010d47df24fdd73fb3001ff9298a64977
Brickgao <brickgao@gmail.com>
2013-09-27 22:43:16 +0800

    fix the feedback for users

--- routes/index.js
+++ routes/index.js
@@ -34,7 +34,26 @@ module.exports = function(app) {
       if(user) {
         return now.get(user.day, user.month, function(is) {
           if(is === true) {
-            return res.send('你已经起床了，你不是逗我玩儿呢吗');
+            var mysearch = new Search();
+            mysearch.get(currentuser.name, function(err, doc) {
+              var s = '';
+              var sh = '';
+              var sm = '';
+              if(doc.hour < 10) {
+                sh = '0' + doc.hour.toString();
+              }
+              else {
+                sh = doc.hour.toString();
+              }
+              if(doc.minutes < 10) {
+                sm = '0' + doc.minutes.toString();
+              }
+              else {
+                sm = doc.minutes.toString();
+              }
+              s = '你已经在' + sh + ':' + sm + '分起床了，你是在调戏我吗？';
+              return res.send(s);
+            });
           }
           if(is === false) {
             currentuser.update(currentuser.name, function(err) {
@@ -45,7 +64,21 @@ module.exports = function(app) {
                 var mysearch = new Search();
                 mysearch.get(currentuser.name, function(err, doc) {
                   var s = '';
-                  s = '亲爱的' + doc.name.toString() + '，\n你的起床时间为：' + doc.hour.toString() + ':' + doc.minutes.toString() + '\n是今天第' + doc.rank.toString() + '个起床的哟!\n成功击败了学校里' + (12460 - doc.rank).toString() + '个还赖在床上的懒虫！\n\n/太阳朝阳唤起我们斗志，\n/奋斗梦想叫醒我们起床！\n\n这是一份属于你的独一无二的起床回执，赶快告诉身边的cupers吧！！~~~~\n\n---------------\n【MrCUP推出早起签到功能，每天早6点至9点，回复数字\"0\"按照提示参与早起签到，快和你的小伙伴们一起看看你的起床排名有木有高端大气上档次！专属石大学子，你加了吗？】';
+                  var sh = '';
+                  var sm = '';
+                  if(doc.hour < 10) {
+                    sh = '0' + doc.hour.toString();
+                  }
+                  else {
+                    sh = doc.hour.toString();
+                  }
+                  if(doc.minutes < 10) {
+                    sm = '0' + doc.minutes.toString();
+                  }
+                  else {
+                    sm = doc.minutes.toString();
+                  }
+                  s = '亲爱的cuper，\n你的起床时间为：' + sh + ':' + sm + '\n是今天第' + doc.rank.toString() + '个起床的哟!\n成功击败了学校里' + (12460 - doc.rank).toString() + '个还赖在床上的懒虫！\n\n/太阳朝阳唤起我们斗志，\n/奋斗梦想叫醒我们起床！\n\n这是一份属于你的独一无二的起床回执，赶快告诉身边的cupers吧！！~~~~\n\n---------------\n【MrCUP正在对签到功能进行升级，现阶段全天开放，无需绑定，直接回复数字0即可测试签到功能。MrCUP欢迎大家给出更多建议与意见！】';
                   return res.send(s);
                 });
               });
@@ -60,7 +93,22 @@ module.exports = function(app) {
           op.op(currentuser.name, function(nrank) {
             var mysearch = new Search();
             mysearch.get(currentuser.name, function(err, doc) {
-              s = '亲爱的' + doc.name.toString() + '，\n你的起床时间为：' + doc.hour.toString() + ':' + doc.minutes.toString() + '\n是今天第' + doc.rank.toString() + '个起床的哟!\n成功击败了学校里' + (12460 - doc.rank).toString() + '个还赖在床上的懒虫！\n\n/太阳朝阳唤起我们斗志，\n/奋斗梦想叫醒我们起床！\n\n这是一份属于你的独一无二的起床回执，赶快告诉身边的cupers吧！！~~~~\n\n---------------\n【MrCUP推出早起签到功能，每天早6点至9点，回复数字\"0\"按照提示参与早起签到，快和你的小伙伴们一起看看你的起床排名有木有高端大气上档次！专属石大学子，你加了吗？】';
+              var s = '';
+              var sh = '';
+              var sm = '';
+              if(doc.hour < 10) {
+                sh = '0' + doc.hour.toString();
+              }
+              else {
+                sh = doc.hour.toString();
+              }
+              if(doc.minutes < 10) {
+                sm = '0' + doc.minutes.toString();
+              }
+              else {
+                sm = doc.minutes.toString();
+              }
+              s = '亲爱的cuper，\n你的起床时间为：' + sh + ':' + sm + '\n是今天第' + doc.rank.toString() + '个起床的哟!\n成功击败了学校里' + (12460 - doc.rank).toString() + '个还赖在床上的懒虫！\n\n/太阳朝阳唤起我们斗志，\n/奋斗梦想叫醒我们起床！\n\n这是一份属于你的独一无二的起床回执，赶快告诉身边的cupers吧！！~~~~\n\n---------------\n【MrCUP正在对签到功能进行升级，现阶段全天开放，无需绑定，直接回复数字0即可测试签到功能。MrCUP欢迎大家给出更多建议与意见！】';
               return res.send(s);
             });
           });

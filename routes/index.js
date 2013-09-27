a10e8651bf904b417cdacc391f4686c325e601b2
Brickgao <brickgao@gmail.com>
2013-09-27 08:35:03 +0800

    remove 'console.log'

--- routes/index.js
+++ routes/index.js
@@ -44,10 +44,8 @@ module.exports = function(app) {
               op.op(currentuser.name, function(nrank) {
                 var mysearch = new Search();
                 mysearch.get(currentuser.name, function(err, doc) {
-                  console.log(doc);
                   var s = '';
                   s = '亲爱的' + doc.name.toString() + '，\n你的起床时间为：' + doc.hour.toString() + ':' + doc.minutes.toString() + '\n是今天第' + doc.rank.toString() + '个起床的哟!\n成功击败了学校里' + (doc.num - 1).toString() + '个还赖在床上的懒虫！\n\n/太阳朝阳唤起我们斗志，\n/奋斗梦想叫醒我们起床！\n\n这是一份属于你的独一无二的起床回执，赶快告诉身边的cupers吧！！~~~~\n\n---------------\n【MrCUP推出早起签到功能，每天早6点至9点，回复数字\"0\"按照提示参与早起签到，快和你的小伙伴们一起看看你的起床排名有木有高端大气上档次！专属石大学子，你加了吗？】';
-                  console.log(s);
                   return res.send(s);
                 });
               });
@@ -62,9 +60,7 @@ module.exports = function(app) {
           op.op(currentuser.name, function(nrank) {
             var mysearch = new Search();
             mysearch.get(currentuser.name, function(err, doc) {
-              console.log(doc);
               s = '亲爱的' + doc.name.toString() + '，\n你的起床时间为：' + doc.hour.toString() + ':' + doc.minutes.toString() + '\n是今天第' + doc.rank.toString() + '个起床的哟!\n成功击败了学校里' + (doc.num - doc.rank).toString() + '个还赖在床上的懒虫！\n\n/太阳朝阳唤起我们斗志，\n/奋斗梦想叫醒我们起床！\n\n这是一份属于你的独一无二的起床回执，赶快告诉身边的cupers吧！！~~~~\n\n---------------\n【MrCUP推出早起签到功能，每天早6点至9点，回复数字\"0\"按照提示参与早起签到，快和你的小伙伴们一起看看你的起床排名有木有高端大气上档次！专属石大学子，你加了吗？】';
-              console.log(s);
               return res.send(s);
             });
           });

d84dfba51f293bf8903ffbb01f6caaba7c29a1a8
Brickgao <brickgao@gmail.com>
2013-10-04 00:08:03 +0800

    fix bug when several parallel req

--- models/rank.js
+++ models/rank.js
@@ -5,34 +5,14 @@ function Rank() {
 
 module.exports = Rank;
 
-Rank.prototype.save = function save(callback) {
+Rank.prototype.refresh = function get(callback) {
   var now = new Date();
   var rank = {
     trick: 'trick',
     month: now.getMonth(),
     day: now.getDate(),
-    rank: 1
+    rank: 0
   }
-  pool.acquire(function(err, db) {
-    if(err) {
-      return callback(err);
-    }
-    db.collection('rank', function(err, collection) {
-      if(err) {
-        pool.release(db);
-        return callback(err);
-      }
-      collection.ensureIndex('trick', {unqiue: true});
-      collection.insert(rank, {safe: true}, function(err, rank) {
-        pool.release(db);
-        callback(err, rank);
-      });
-    });
-  });
-}
-
-
-Rank.prototype.get = function get(callback) {
   pool.acquire(function(err, db) {
     if (err) {
       return callback(err);
@@ -43,42 +23,35 @@ Rank.prototype.get = function get(callback) {
         return callback(err);
       }
       collection.findOne({trick: 'trick'}, function(err, doc) {
-        pool.release(db);
         if (doc) {
-          callback(err, doc);
+          if(doc.month != now.getMonth() || doc.day != now.getDate()) {
+            collection.update({
+              trick: 'trick'
+            }, {
+              $set: {
+                month: now.getMonth(),
+                day: now.getDate(),
+                rank: 0
+              }
+            }, function (err, result) {
+              pool.release(db);
+              if (err) {
+                return callback(err);
+              }
+              callback(null);
+            });
+          }
+          else {
+            pool.release(db);
+            callback(err);
+          }
         } else {
-          callback(err, null);
+          collection.ensureIndex('trick', {unqiue: true});
+          collection.insert(rank, {safe: true}, function(err) {
+            pool.release(db);
+            callback(err);
+          });
         }
-      });
-    });
-  });
-}
-
-Rank.prototype.erase = function erase(callback) {
-  var now = new Date();
-  pool.acquire(function(err, db) {
-    if(err) {
-      return callback(err);
-    }
-    db.collection('rank', function(err, collection) {
-      if(err) {
-        pool.release(db);
-        return callback(err);
-      }
-      collection.update({
-        trick: 'trick'
-      }, {
-        $set: {
-          month: now.getMonth(),
-          day: now.getDate(),
-          rank: 1
-        }
-      }, function (err, result) {
-        pool.release(db);
-        if (err) {
-          return callback(err);
-        }
-        callback(null);
       });
     });
   });
--- models/rankop.js
+++ models/rankop.js
@@ -8,45 +8,13 @@ module.exports = RankOP;
 RankOP.prototype.op = function op(username, callback) {
   var now = new Date();
   var currentrank = new Rank();
-  currentrank.get(function(err, ndoc) {
-    if(ndoc) {
-      var check = {
-        day: now.getDate(),
-        month: now.getMonth()
-      }
-      if(ndoc.day === check.day && ndoc.month === check.month) {
-        return currentrank.update(function(err, nrank) {
-          var user = require('./wake.js');
-          var nuser = new user();
-          nuser.updaterank(username, nrank, function(err) {
-            callback(nrank);
-          });
-        });
-      }
-      else {
-        ret = 1;
-        return currentrank.erase(function(err) {
-          var user = require('./wake.js');
-          var nuser = new user();
-          nuser.updaterank(username, 1, function(err) {
-            currentrank.erase(function(err) {
-              callback(1);
-            });
-          });
-        });
-      }
-    }
-    else {
-      ret = 1;
-      return currentrank.save(function(err) {
-        var user = require('./wake.js');
-        var nuser = new user();
-        nuser.updaterank(username, 1, function(err) {
-          currentrank.save(function(err) {
-            callback(1);
-          });
-        });
+  currentrank.refresh(function(err) {
+    return currentrank.update(function(err, nrank) {
+      var user = require('./wake.js');
+      var nuser = new user();
+      nuser.updaterank(username, nrank, function(err) {
+        callback(nrank);
       });
-    }
+    });
   });
 }
